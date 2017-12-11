1a8798c35d735ee4bee3f83b3fe53caa83a4e082
Hannah Tucker McLellan <hannahjt.703@gmail.com>
2017-12-11 10:58:44 +0000

    searching the list items for class attribute

--- js/app.js
+++ js/app.js
@@ -2,59 +2,72 @@ $(() => {
 
   const $counter = $('.counter');
   const $icons = $('li');
-  const $icon2 = $('li.icon1');
-  const $icon3 = $('li.icon1');
-  const $icon4 = $('li.icon1');
-  const $icon5 = $('li.icon1');
-  const $icon6 = $('li.icon1');
-  const $icon7 = $('li.icon1');
-  const $icon8 = $('li.icon1');
-  const $icon9 = $('li.icon1');
-  const $icon10 = $('li.icon1');
-  const $icon11 = $('li.icon1');
-  const $icon12 = $('li.icon1');
-  const $icon13 = $('li.icon1');
-  const $icon14 = $('li.icon1');
-  const $icon15 = $('li.icon1');
-  const $icon16 = $('li.icon1');
-  const $icon17 = $('li.icon1');
-  const $icon18 = $('li.icon1');
-  const $icon19 = $('li.icon1');
-  const $icon20 = $('li.icon1');
+
   // Up arrow	38
   // Down arrow	40
   // Left arrow	37
   // Right arrow	39
 
   const gridUK = {
-    $icon1: true,
-    $icon2: false,
-    $icon3: false,
-    $icon4: false,
-    $icon5: false,
-    $icon6: true,
-    $icon7: true,
-    $icon8: true,
-    $icon9: true,
-    $icon10: true,
-    $icon11: false,
-    $icon12: false,
-    $icon13: false,
-    $icon14: false,
-    $icon15: true,
-    $icon16: true,
-    $icon17: true,
-    $icon18: true,
-    $icon19: true,
-    $icon20: true
-  };
-
-  function showCounter(){
-    for (let i = 0; i < array.length; i++) {
-      if (clickedIcon === true) {
-        $counter.show();
-      }
-    }
+    icon1: true,
+    icon2: false,
+    icon3: false,
+    icon4: false,
+    icon5: false,
+    icon6: true,
+    icon7: true,
+    icon8: true,
+    icon9: true,
+    icon10: true,
+    icon11: false,
+    icon12: false,
+    icon13: false,
+    icon14: false,
+    icon15: true,
+    icon16: true,
+    icon17: true,
+    icon18: true,
+    icon19: true,
+    icon20: true
   }
+
+  // change class vs check attribute -> finding out which image is used
+
+  //   function iconTargetted(e) {
+  //     // for (let i = 0; i < gridUK.length; i++) {
+  //     //   if ($gridUK[i].target()
+  //     // }
+  //     e.target.style.visibility = 'hidden';
+  //   }
+  //
+  //   function hideIcon() {
+  //     if () {
+  //       $icon[i].hide();
+  //     }
+  //   }
+  //
+  //   function showCounter(){
+  //     for (let i = 0; i < gridUK.length; i++) {
+  //       if (i === true) {
+  //         $counter.show();
+  //       } else hideIcon();
+  //     }
+  //   }
+  //
+  // });
+
+  // I need to listen for a click
+  // then, if clicked, I need to hide icon and show showCounter
+  //icons on click, => check class => store in variable
+  // if true do x, if false do y
+  //
+
+  $icons.on('click', function(e) {
+    const whichClass = $(e.target).attr('class');
+    console.log(whichClass);
+    if (gridUK[whichClass]) {
+      gridUK[whichClass].removeClass( 'icons' ).addClass( 'counter' );
+    }
+  });
 
 });
