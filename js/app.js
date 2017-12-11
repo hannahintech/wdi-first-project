face36bfccf810610b7553523de46260dc476623
Hannah Tucker McLellan <hannahjt.703@gmail.com>
2017-12-11 09:31:52 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+$(() => {
+
+  const $counter = $('.counter');
+  const $icons = $('li');
+  const $icon2 = $('li.icon1');
+  const $icon3 = $('li.icon1');
+  const $icon4 = $('li.icon1');
+  const $icon5 = $('li.icon1');
+  const $icon6 = $('li.icon1');
+  const $icon7 = $('li.icon1');
+  const $icon8 = $('li.icon1');
+  const $icon9 = $('li.icon1');
+  const $icon10 = $('li.icon1');
+  const $icon11 = $('li.icon1');
+  const $icon12 = $('li.icon1');
+  const $icon13 = $('li.icon1');
+  const $icon14 = $('li.icon1');
+  const $icon15 = $('li.icon1');
+  const $icon16 = $('li.icon1');
+  const $icon17 = $('li.icon1');
+  const $icon18 = $('li.icon1');
+  const $icon19 = $('li.icon1');
+  const $icon20 = $('li.icon1');
+  // Up arrow	38
+  // Down arrow	40
+  // Left arrow	37
+  // Right arrow	39
+
+  const gridUK = {
+    $icon1: true,
+    $icon2: false,
+    $icon3: false,
+    $icon4: false,
+    $icon5: false,
+    $icon6: true,
+    $icon7: true,
+    $icon8: true,
+    $icon9: true,
+    $icon10: true,
+    $icon11: false,
+    $icon12: false,
+    $icon13: false,
+    $icon14: false,
+    $icon15: true,
+    $icon16: true,
+    $icon17: true,
+    $icon18: true,
+    $icon19: true,
+    $icon20: true
+  };
+
+  function showCounter(){
+    for (let i = 0; i < array.length; i++) {
+      if (clickedIcon === true) {
+        $counter.show();
+      }
+    }
+  }
+
+});
